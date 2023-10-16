aa5989f6d8ad4c32c15dc7d7bcb118ef6c120ea0
deds129 <nik.chudinov129@mail.ru>
2023-10-16 16:34:35 +0300

    fix return type in controller

--- src/main/java/ru/deds/mybatisdemo/controller/OrderController.java
+++ src/main/java/ru/deds/mybatisdemo/controller/OrderController.java
@@ -8,28 +8,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.deds.mybatisdemo.model.Order;
-import ru.deds.mybatisdemo.model.User;
 
 @RestController("/batis-api/orders")
 public class OrderController {
     
     @GetMapping("{id}")
-    public User getOrderById(@PathVariable("id") Long id){
+    public Order getOrderById(@PathVariable("id") Long id) {
         return null;
     }
 
     @GetMapping()
-    public User getAllOrders(){
+    public Order getAllOrders(){
         return null;
     }
 
     @PostMapping()
-    public User addOrder(@RequestBody Order order){
+    public Order addOrder(@RequestBody Order order){
         return null;
     }
 
     @DeleteMapping()
-    public User deleteOrderById(@PathVariable("id") Long id){
+    public Order deleteOrderById(@PathVariable("id") Long id){
         return null;
     }
     
